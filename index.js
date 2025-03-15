28daf0e74e3f5fb787f2f6cbe9c48c2df52daf89
davidjyacus <143612899+davidjyacus@users.noreply.github.com>
2025-03-15 13:24:41 +0100

    reset button

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ function increment() {
 }
 
 function save() {
-    let prevEntry = count + " – "
+    let prevEntry = count + " - "
     saveEl.textContent += prevEntry
     console.log(count)
     countEl.textContent = 1
     count = 1
+}
+
+function reset() {
+    count = 1
+    countEl.textContent = count
+    saveEl.textContent = "MULTIMEASURE GROUPS: "
 }
  
